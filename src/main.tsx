1f4d7f6239f6312b96d36c2a3eab0a1fc674ca2a
Polarblitz1 <lesagejake1@gmail.com>
2026-05-03 13:41:34 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,4 @@
-import { saveScore, getLeaderboard } from './leaderboard'
-
-// Example: When the bird hits a pipe
-function gameOver(finalScore) {
-    const playerName = prompt("Enter your name for the leaderboard:");
-    saveScore(playerName, finalScore); import { createRoot } from "react-dom/client";
+; import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
